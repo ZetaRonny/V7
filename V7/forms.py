829d9fef002c68f5fb40e49c06f5c6d16531c1e5
ronny archer <variantsvn@gmail.com>
2020-06-14 11:06:30 -0700

    login and registration, some serializers and products

---
+++ V7/forms.py
@@ -0,0 +1,34 @@
+# forms.py
+
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class LoginForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+	username = forms.CharField()
+	email = forms.CharField()
+	password = forms.CharField()
+	confirm_password = forms.CharField()
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+		qs = User.objects.filter(username=username)
+		if qs.exists():
+			return forms.ValidationError("username exists select new one")
+		return username
+
+	def clean(self):
+		data =  self.cleaned_data
+		password = self.cleaned_data.get('password')
+		confirm_password = self.cleaned_data.get('confirm_password')
+		if password != confirm_password:
+			raise forms.ValidationError('passwords must match')
+		return data
+
+
+
--- V7/views.py
+++ V7/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login, get_user_model
 
+from .forms import LoginForm , RegisterForm
 
 def index(request):
 	context = {"title":"V7",} 
@@ -15,3 +17,33 @@ def contact(request):
 	"title":"Contact",
 	} 
 	return render(request, 'base_template.html', context)
+
+
+def login_page(request):
+	form = LoginForm(request.POST or None)
+	context = {"form": form}
+	if form.is_valid():
+		username = form.cleaned_data.get("username")
+		password = form.cleaned_data.get("password")
+		user = authenticate(request,username=username, password=password)
+		if user is not None:
+			login(request,user)
+			context['form'] = LoginForm()
+			# return redirect("/admin")
+		else:
+			print('err')
+	return render(request,'auth/login_template.html', context)
+
+
+def register_page(request):
+	user = get_user_model()
+	form = RegisterForm(request.POST or None)
+	context = {"form": form}
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+		email = form.cleaned_data.get('email')
+		confirm_password = form.cleaned_data.get('confirm_password')
+		user.objects.create_user(username, email, password)  
+		# authenticate email 
+	return render(request,'auth/register_template.html', context)
