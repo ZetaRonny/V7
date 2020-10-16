6824cacc84a4f716a527b4edaab998827461e355
ronny archer <variantsvn@gmail.com>
2020-10-16 17:07:31 -0400

    update to settings dot py

--- V7/settings.py
+++ V7/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'V7/static_compiled'
 
 STATIC_ROOT ='/static_root/ '
 
