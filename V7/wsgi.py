81abc7986b965266e280037c875679c30fcbb4b8
ronny archer <variantsvn@gmail.com>
2021-01-05 11:31:39 -0500

    wsgi stuff

--- V7/wsgi.py
+++ V7/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'V7.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'V7.settings.production')
 
 application = get_wsgi_application()
